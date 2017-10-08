902403831a6127d3c6c5fd7324093aee52392b28
Tudor Manole <tyro12@gmail.com>
2017-10-08 13:19:36 -0400

    initial before i start messing things up

--- src/components/Devices.js
+++ src/components/Devices.js
@@ -4,44 +4,52 @@ import {  BrowserRouter as Router, Route, Link } from 'react-router-dom'
 var PropTypes = require('prop-types');
 var api = require('../utils/api');
 
-class EditModal extends React.Component {
+class DeviceControl extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedDevice: null,
-      showModal: false
+      device: props.device,
+      showEditModal: false
     };
+    this.showEditModal = this.showEditModal.bind(this);
   }
 
-  close = () => {
-    this.setState({ showModal: false });
-    console.log(this.state.showModal);
+  closeEdit() {
+    this.setState({ showEditModal: false });
   }
 
-  open = () => {
-    this.setState({ showModal: true });
-    console.log(this.state.showModal);
+  openEdit() {
+    this.setState({ showEditModal: true });
   }
 
   render() {
     return (
       <div>
-        <Button bsSize="small" bsStyle="primary" onClick={this.open}>Edit</Button>
-        <Modal show={this.state.showModal} onHide={this.close}>
+        <Button bsStyle="primary" bsSize="small" onClick={this.openEdit}>Edit</Button>
+​
+        <Modal show={this.state.showEditModal} onHide={this.closeEdit}>
           <Modal.Header closeButton>
-            <Modal.Title>Edit Device</Modal.Title>
+            <Modal.Title>Editing Device {this.state.device.hostname}</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            <h4>Text in a modal</h4>
+            <p>Hostname: {this.state.device.hostname}</p>
+            <p>Model: {this.state.device.model}</p>
+            <p>Serial: {this.state.device.serial}</p>
+            <p>Mgmt. IP: {this.state.device.mgmtIP}</p>
+            <p>Vendor: {this.state.device.vendor}</p>
+            <p>Rack: {this.state.device.rack}</p>
+            <p>Lab Module: {this.state.device.labmodule}</p>
+            <p>MRV: {this.state.device.mrv}</p>
           </Modal.Body>
           <Modal.Footer>
-            <Button onClick={this.close}>Close</Button>
+            <Button onClick={this.closeEdit}>Close</Button>
           </Modal.Footer>
         </Modal>
       </div>
-    )
+    );
   }
 }
+
 
 class Device extends React.Component {
   constructor(props) {
@@ -54,20 +62,18 @@ class Device extends React.Component {
   render() {
     return (
       <tr key={this.state.device.hostname}>
-        <td>{this.state.device.hostname}</td>
-        <td>{this.state.device.model}</td>
-        <td>{this.state.device.serial}</td>
-        <td>{this.state.device.mgmtIP}</td>
-        <td>{this.state.device.vendor}</td>
-        <td>{this.state.device.rack}</td>
-        <td>{this.state.device.labmodule}</td>
-        <td>{this.state.device.mrv}</td>
-        <td>
-          <ButtonToolbar>
-              <EditModal device={this.state.device} />
-              <Button bsSize="small" bsStyle="danger" onClick={this.props.toggleDelete}>Delete</Button>
-          </ButtonToolbar>
-        </td>
+        <p>{this.state.device.hostname}</p>
+        <p>{this.state.device.model}</p>
+        <p>{this.state.device.serial}</p>
+        <p>{this.state.device.mgmtIP}</p>
+        <p>{this.state.device.vendor}</p>
+        <p>{this.state.device.rack}</p>
+        <p>{this.state.device.labmodule}</p>
+        <p>{this.state.device.mrv}</p>
+        <p>
+            <Button bsSize="small" bsStyle="primary" onClick={() => this.props.showEditModal(this.state.device)}>Edit</Button>
+            <Button bsSize="small" bsStyle="danger" onClick={() => this.props.showDeleteModal(this.state.device)}>Delete</Button>
+        </p>
       </tr>
     )
   }
@@ -78,11 +84,12 @@ class Devices extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      devices: null,
-      selectedDevice: null
+      devices: null
     };
+    this.showEditModal = this.showEditModal.bind(this);
+    this.showDeleteModal = this.showDeleteModal.bind(this);
   }
-  componentDidMount() {
+  componenpidMount() {
     api.fetchDevices()
       .then((devices) => {
         this.setState(function () {
@@ -91,6 +98,27 @@ class Devices extends React.Component {
           }
         });
       });
+  }
+
+  showEditModal(device) {
+    console.log("SHOW EDIT MODAL FOR " + device.hostname);
+    return (
+      <Modal show={true} onHide={this.close}>
+          <Modal.Header closeButton>
+            <Modal.Title>Editing Device `"{device.hostname}"`</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <h4>Text in a modal</h4>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.close}>Close</Button>
+          </Modal.Footer>
+        </Modal>
+    )
+  }
+
+  showDeleteModal(device) {
+    console.log("SHOW DELETE MODAL FOR " + device.hostname);
   }
 
   render() {
@@ -104,31 +132,35 @@ class Devices extends React.Component {
         {!this.state.devices
           ? <p>LOADING!</p>
           : (
-              <Table striped bordered condensed hover>
-                <thead>
-                  <tr>
-                    <th>Hostname</th>
-                    <th>Model</th>
-                    <th>Serial</th>
-                    <th>Mgmt. IP</th>
-                    <th>Vendor</th>
-                    <th>Rack</th>
-                    <th>Lab Module</th>
-                    <th>MRV</th>
-                    <th>Controls</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {this.state.devices.map((device) => {
-                    return (
-                      <Device
-                        key={device.hostname}
-                        device={device}
-                      />
-                    )
-                  })}
-                </tbody>
-              </Table>
+              <div>
+                <Table striped bordered condensed hover>
+                  <thead>
+                    <tr>
+                      <th>Hostname</th>
+                      <th>Model</th>
+                      <th>Serial</th>
+                      <th>Mgmt. IP</th>
+                      <th>Vendor</th>
+                      <th>Rack</th>
+                      <th>Lab Module</th>
+                      <th>MRV</th>
+                      <th>Controls</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {this.state.devices.map((device) => {
+                      return (
+                        <Device
+                          key={device.hostname}
+                          device={device}
+                          showEditModal={this.showEditModal}
+                          showDeleteModal={this.showDeleteModal}
+                        />
+                      )
+                    })}
+                  </tbody>
+                </Table>
+              </div>
           )
         }
       </div>
