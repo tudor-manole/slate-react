44199df027355b8a8c018a3ebc171b0c9280a76e
Tudor Manole <tudor@manole.ca>
2017-12-15 11:35:00 -0500

    keeping more unknown changes

--- src/utils/api.js
+++ src/utils/api.js
@@ -9,5 +9,14 @@ module.exports = {
         // console.log(response.data);
         return response.data;
       });
+  },
+  updateDevice: function (newDevice) {
+    return axios.put( window.encodeURI(`http://localhost:8000/devices/${newDevice.hostname}`),
+    { hostname: newDevice.hostname, model: newDevice.model, serial: newDevice.serial, mgmtIP: newDevice.mgmtIP,
+        vendor: newDevice.vendor, mrv: newDevice.mrv, rack: newDevice.rack, labmodule: newDevice.labmodule })
+      .then(function (response) {
+        return response.data;
+      });
+
   }
 };
